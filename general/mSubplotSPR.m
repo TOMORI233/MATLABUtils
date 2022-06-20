1fc1c495848e01f9a24e933a2ddc2d438c1a6410
TOMORI233 <ihatewo@qq.com>
2022-06-20 15:34:28 +0800

    update 20220620 by xhx

--- general/mSubplotSPR.m
+++ general/mSubplotSPR.m
@@ -1,26 +1,16 @@
-function mAxe = mSubplot(Fig, row, col, index, nSize, margins, paddings)
-    narginchk(4, 7);
-
-    if nargin < 5
-        nSize = [1, 1];
-    end
-
-    if nargin < 6 || isempty(margins)
-        margins = 0.01 * ones(1, 4);
-    end
-
-    if nargin < 7 || isempty(paddings)
+function mAxe = mSubplotSPR(row, col, index, margins, paddings)
+    narginchk(3, 5);
+    nSize = [1, 1];
+    if nargin == 3
+        paddings = 0.01 * ones(1, 4);
+        margins = 0.07 * ones(1, 4);
+    elseif nargin == 4
         paddings = 0.01 * ones(1, 4);
     end
 
     % nSize = [nX, nY]
     nX = nSize(1);
-
-    if length(nSize) == 1
-        nY = 1;
-    else
-        nY = nSize(2);
-    end
+    nY = nSize(2);
 
     % paddings or margins is [Left, Right, Bottom, Top]
     divWidth = (1 - paddings(1) - paddings(2)) / col;
@@ -40,12 +30,19 @@ function mAxe = mSubplot(Fig, row, col, index, nSize, margins, paddings)
     divX = paddings(1) + divWidth * (cIndex - 1);
     divY = 1 - paddings(4) - divHeight * (rIndex + nY - 1);
     axeX = divX + margins(1) * divWidth * nX;
-    axeY = divY + margins(3) * divHeight * nY;
-    axeWidth = (1 - margins(1) - margins(2)) * divWidth * nX;
-    axeHeight = (1 - margins(3) - margins(4)) * divHeight * nY;
+    axeY = divY + margins(3)*1.5 * divHeight * nY;
 
+    nX = length(unique(axeX)); % mSubplot(Fig,2,2,[1 2]);
+    nY = length(unique(axeY));
+
+    axeWidth = (1 - margins(1) - margins(2)) * divWidth * nX + 0.5 * (nX-1) * margins(1);
+    axeHeight = (1 - margins(3)*1.5 - margins(4)*1.5) * divHeight * nY + 0.5 * (nY-1) * margins(3);
+
+    axeX = axeX(1);
+    axeY = axeY(1);
+    
 %     divAxe = axes(Fig, "Position", [divX, divY, divWidth * nX, divHeight * nY], "Box", "on");
-    mAxe = axes(Fig, "Position", [axeX, axeY, axeWidth, axeHeight], "Box", "on");
+    mAxe = axes(gcf, "Position", [axeX, axeY, axeWidth, axeHeight], "Box", "off");
 
     return;
 end
