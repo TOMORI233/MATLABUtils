f15c2ba0a5df2db7705b92f664947ae7e63ee9e6
TOMORI233 <ihatewo@qq.com>
2022-10-28 23:22:15 +0800

    update rowFcn

--- data structures/matrix/rowFcn.m
+++ data structures/matrix/rowFcn.m
@@ -8,20 +8,21 @@ function varargout = rowFcn(fcn, A, varargin)
     % Output:
     %     Ouputs of fcn with nRows=size(A, 1)
 
-    mInputParser = inputParser;
-    mInputParser.addRequired("fcn", @(x) validateattributes(x, 'function_handle', {'numel', 1}));
-    mInputParser.addRequired("A", @(x) validateattributes(x, 'numeric', {'2d'}));
+    mIp = inputParser;
+    mIp.addRequired("fcn", @(x) validateattributes(x, 'function_handle', {'numel', 1}));
+    mIp.addRequired("A", @(x) validateattributes(x, 'numeric', {'2d'}));
 
     for n = 1:sum(cellfun(@isnumeric, varargin))
         eval(['B', num2str(n), '=varargin{', num2str(n), '};']);
-        mInputParser.addOptional(eval(['"B', num2str(n), '"']), [], @(x) validateattributes(x, 'numeric', {'size', [size(A, 1), NaN]}));
+        mIp.addOptional(eval(['"B', num2str(n), '"']), [], @(x) validateattributes(x, 'numeric', {'size', [size(A, 1), NaN]}));
     end
 
-    mInputParser.addParameter("UniformOutput", true, @islogical);
-    mInputParser.parse(fcn, A, varargin{:});
+    mIp.addParameter("UniformOutput", true, @islogical);
+    mIp.parse(fcn, A, varargin{:});
 
-    singleRowFcnHandle = @(fcn, A, varargin) @(row) fcn(A(row, :), varargin{:}(row, :));
-    rowFcnHandle = @(fcn, A, varargin) arrayfun(singleRowFcnHandle(fcn, A, varargin{:}), 1:size(A, 1), "UniformOutput", mInputParser.Results.UniformOutput);
+    commaSeparateIndexing = @(C, idx) struct("output", cellfun(@(x) x(idx, :), C, "UniformOutput", false));
+    singleRowFcnHandle = @(fcn, A, varargin) @(row) fcn(A(row, :), commaSeparateIndexing(varargin, row).output);
+    rowFcnHandle = @(fcn, A, varargin) arrayfun(singleRowFcnHandle(fcn, A, varargin{:}), 1:size(A, 1), "UniformOutput", mIp.Results.UniformOutput);
     [varargout{1:nargout}] = rowFcnHandle(fcn, A, varargin{cellfun(@isnumeric, varargin)});
     varargout = cellfun(@(x) x', varargout, "UniformOutput", false);
     return;
