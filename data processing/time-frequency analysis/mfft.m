2c6391c3334fae95c7aa28d62d6fa0ae31aaf7ef
TOMORI233 <ihatewo@qq.com>
2022-11-04 16:57:56 +0800

    update 20221104 by xhx

--- data processing/time-frequency analysis/mfft.m
+++ data processing/time-frequency analysis/mfft.m
@@ -1,25 +1,26 @@
-function [A, f, Aoi] = mfft(X, fs, N, dim, foi)
+function [A, f, Aoi] = mfft(X, fs, varargin)
     % Description: return single-sided amplitude spectrum A of data X
     % Input:
     %     X: data
-    %     fs: sampling frequency
-    %     N: N-point fft
-    %     dim: 1-along column, 2-along row
-    %     foi: frequency range of interest, one- or two element vector
+    %     fs: sampling frequency, Hz
+    %     N: N-point fft (default: data length)
+    %     dim: 1 for data of [nSample, nCh], 2 for data of [nCh, nSample] (default: 2)
+    %     foi: frequency range of interest, one- or two-element vector
     % Output:
     %     A: single-sided amplitude spectrum
     %     f: frequency vector of N-point single-sided fft
     %     Aoi: amplitude of foi
 
-    narginchk(2, 5);
-
-    if nargin < 3
-        N = [];
-    end
-
-    if nargin < 4 || isempty(dim)
-        dim = 2;
-    end
+    mIp = inputParser;
+    mIp.addRequired("X", @(x) validateattributes(x, 'numeric', {'2d'}));
+    mIp.addRequired("fs", @(x) validateattributes(x, 'numeric', {'numel', 1, 'positive'}));
+    mIp.addOptional("N", [], @(x) validateattributes(x, 'numeric', {'numel', 1, 'positive', 'integer'}));
+    mIp.addOptional("dim", 2, @(x) ismember(x, [1, 2]));
+    mIp.addOptional("foi", [], @(x) validateattributes(x, 'numeric', {'2d', 'increasing', 'positive', "<=", fs/2}));
+    mIp.parse(X, fs, varargin{:});
+    N = mIp.Results.N;
+    dim = mIp.Results.dim;
+    foi = mIp.Results.foi;
 
     X = permute(X, [3 - dim, dim]);
     Y = fft(X, N, 2);
