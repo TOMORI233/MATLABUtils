9afabe08448ec511fedda54d5c976a732074198f
TOMORI233 <ihatewo@qq.com>
2022-11-25 15:17:04 +0800

    update 20221125 by xhx

--- data structures/struct/validatestruct.m
+++ data structures/struct/validatestruct.m
@@ -1,17 +1,28 @@
-function pass = validatestruct(s, varargin)
-    % Description: validate each field of a struct
+function [pass, msg] = validatestruct(s, varargin)
+    % Description: validate each field of a struct vector
+    % Input:
+    %     s: a struct vector
+    %     fieldname: field name of [s] to validate
+    %     validatefcn: validate function handle
+    % Output:
+    %     pass: pass validation or not
+    %     msg: validation failure message
 
     fIdx = find(cellfun(@(x) ischar(x) || isstring(x), varargin));
     msg = 'Validation Failed: ';
     pass = true;
 
-    for n = 1:length(fIdx)
+    for sIndex = 1:length(s)
 
-        try
-            varargin{fIdx(n) + 1}(s.(varargin{fIdx(n)}));
-        catch e
-            msg = [msg, newline, char(varargin{fIdx(n)}), ': ', char(e.message)];
-            pass = false;
+        for n = 1:length(fIdx)
+    
+            try
+                varargin{fIdx(n) + 1}(s(sIndex).(varargin{fIdx(n)}));
+            catch e
+                msg = [msg, newline, num2str(sIndex), ' - ', char(varargin{fIdx(n)}), ': ', char(e.message)];
+                pass = false;
+            end
+    
         end
 
     end
