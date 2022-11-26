ccb98c6ab59a3e5c993157ccdb8cbba7c9c00abc
TOMORI233 <ihatewo@qq.com>
2022-11-26 23:18:00 +0800

    update mSubplot by adding alignment input

--- data structures/struct/getOr.m
+++ data structures/struct/getOr.m
@@ -1,28 +1,35 @@
-function v = getOr(s, field, default)
-    %   getOr Returns the structure field or a default if either don't exist
-    %   v = getOr(s, field, [default]) returns the 'field' of the structure 's'
-    %   or 'default' if the structure is empty of the field does not exist. If
-    %   default is not specified it defaults to []. 'field' can also be a cell
-    %   array of fields, in which case it will check for all of them and return
-    %   the value of the first field that exists, if any (otherwise the default
-    %   value).
+function v = getOr(s, field, default, emptyOpt)
+%   getOr Returns the structure field or a default if either don't exist
+%   v = getOr(s, field, [default]) returns the 'field' of the structure 's'
+%   or 'default' if the structure is empty of the field does not exist. If
+%   default is not specified it defaults to []. 'field' can also be a cell
+%   array of fields, in which case it will check for all of them and return
+%   the value of the first field that exists, if any (otherwise the default
+%   value).
+narginchk(2, 4);
 
-    if nargin < 3
-        default = [];
-    end
+if nargin < 3
+    default = [];
+end
 
-    fieldExists = isfield(s, field);
+if nargin < 4
+    emptyOpt = false;
+end
 
-    if any(fieldExists)
+fieldExists = isfield(s, field);
 
-        if iscellstr(field)
-            v = s.(field{find(fieldExists, 1)});
-        else
-            v = s.(field);
-        end
-
+if any(fieldExists)
+    if ~emptyOpt
+        v = s.(field{find(fieldExists, 1)});
     else
-        v = default;
+        if all(cellfun(@isempty, {s.(field)}))
+            v = default;
+        else
+            v = s.(field{find(fieldExists, 1)});
+        end
     end
+else
+    v = default;
+end
 
 end
--- plot/generateColorGrad.m
+++ plot/generateColorGrad.m
@@ -1,13 +1,12 @@
 function [colorOutput, colorGrad] = generateColorGrad(n,method,varargin)
-
 % if only one input n and method, the colorOutput will include all color types, for
-% each color type, the color gradation if continuous. 
+% each color type, the color gradation if continuous.
 % for example: res = generateColorGrad(16).
 % method: 'rgb' or 'hex'
 
-% you can give more input, varargin can be 'blue','red', etc, followed by index, 
+% you can give more input, varargin can be 'blue','red', etc, followed by index,
 % for example: generateColorGrad(16,'orange',[1:5]),
-% the 1 to 5 of colorOutput will be orange gradation. 
+% the 1 to 5 of colorOutput will be orange gradation.
 % Be careful, the length of num must less than 6
 
 % For more info of color gradation, go to this website:
@@ -44,61 +43,59 @@ end
 
 colorGrad = easyStruct({'blueGrad','greenGrad','orangeGrad','redGrad','blackGrad'},colorBuffer');
 
-
 [colorType colorNum] = size(colorBuffer);
 
-%% 
+%%
 if mod(nargin,2) == 1
     error('the number of input is incorrect!');
 elseif n > numel(colorBuffer)
     error(['the input exceeds color pool size!']);
 elseif nargin < 3
-     sz = size(colorBuffer);
-     Idx = [];
-     for N = 1:colorType
+    sz = size(colorBuffer);
+    Idx = [];
+    for N = 1:colorType
         Idx = [Idx N:colorType:n];
-     end
-     colorOutput = colorBuffer(Idx);
+    end
+    colorOutput = colorBuffer(Idx);
 else
     calN = sum(cellfun(@length,varargin(2:2:length(varargin))));
     if n~= calN
         error(['the sum of color index do not equal to n!']);
     end
-     for ii = 1:2:length(varargin)
-         Idx = varargin{ii+1};
-         if length(Idx) > length(colorGrad)
-             error(['the index length of ' varargin{ii} ' exceeds color pool size!']);
-         end
-         colorOutput(Idx) = {colorGrad(1:length(Idx)).([varargin{ii} 'Grad'])};
-     end
+    for ii = 1:2:length(varargin)
+        Idx = varargin{ii+1};
+        if length(Idx) > length(colorGrad)
+            error(['the index length of ' varargin{ii} ' exceeds color pool size!']);
+        end
+        colorOutput(Idx) = {colorGrad(1:length(Idx)).([varargin{ii} 'Grad'])};
+    end
 end
 
 
 
 %% easyStruct
-function res = easyStruct(fieldName,fieldVal)
-evalstr=['res=struct('];
-%% if certain field is double defined, delete the old one
-deleteIdx= [];
-fieldN = unique(fieldName);
-if length(fieldN) ~= length(fieldName)
-    for i = 1:length(fieldN)
-        if sum(strcmp(fieldName,fieldN{i})) > 1
-            idx = find(strcmp(fieldName,fieldN{i}));
-            reserveIdx = max(find(strcmp(fieldName,fieldN{i})));
-            deleteIdx = [deleteIdx ; idx(idx~=reserveIdx)]'
+    function res = easyStruct(fieldName,fieldVal)
+    evalstr=['res=struct('];
+    %% if certain field is double defined, delete the old one
+    deleteIdx= [];
+    fieldN = unique(fieldName);
+    if length(fieldN) ~= length(fieldName)
+        for i = 1:length(fieldN)
+            if sum(strcmp(fieldName,fieldN{i})) > 1
+                idx = find(strcmp(fieldName,fieldN{i}));
+                reserveIdx = max(find(strcmp(fieldName,fieldN{i})));
+                deleteIdx = [deleteIdx ; idx(idx~=reserveIdx)]'
+            end
         end
     end
-end
-fieldName(deleteIdx) = [];
-fieldVal(:,deleteIdx) = [];
+    fieldName(deleteIdx) = [];
+    fieldVal(:,deleteIdx) = [];
 
-for Paranum=1:length(fieldName)
-    evalstr=[ evalstr '''' fieldName{Paranum} ''',' 'fieldVal(:,' num2str(Paranum) '),'];
+    for Paranum=1:length(fieldName)
+        evalstr=[ evalstr '''' fieldName{Paranum} ''',' 'fieldVal(:,' num2str(Paranum) '),'];
+    end
+    evalstr(end)=[')'];
+    evalstr=[evalstr ';'];
+    eval(evalstr);
+    end
 end
-evalstr(end)=[')'];
-evalstr=[evalstr ';'];
-eval(evalstr);
-end
-end
-
