0253a704e7e96cc1cada0ac38df3ea0f99738639
418-SPR <11815013@zju.edu.cn>
2022-12-12 15:39:07 +0800

    20221212spr

--- toolbox API/FieldTrip/ft_setPath2Top.m
+++ toolbox API/FieldTrip/ft_setPath2Top.m
@@ -3,7 +3,8 @@ function ft_setPath2Top
 try
     % find existing FieldTrip paths
     startupPath = which("startup", "-all");
-    mPath = string(regexp(startupPath{end}, '.*FieldTrip', 'match'));
+    mPath = regexp(string(startupPath), '.*FieldTrip', 'match');
+    mPath = string(mPath{~cellfun(@isempty, mPath)});
     temp = dir(strcat(mPath, "\**\*"));
     mfolders = string({temp([temp.isdir]').folder}');
     p = string(strsplit(path, ";"))';
