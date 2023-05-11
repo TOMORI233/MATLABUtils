8bee4e096951a9a8c46885076fe7872a2cec933c
TOMORI233 <ihatewo@qq.com>
2023-05-11 15:59:28 +0800

    debug

--- data structures/matrix/rowFcn.m
+++ data structures/matrix/rowFcn.m
@@ -13,15 +13,15 @@ function varargout = rowFcn(fcn, A, varargin)
     %% Validation
     mIp = inputParser;
     mIp.addRequired("fcn", @(x) validateattributes(x, 'function_handle', {'scalar'}));
-    mIp.addRequired("A", @(x) validateattributes(x, {'numeric', 'string', 'char', 'cell', 'logical'}, {'2d'}));
-    bIdx = find(cellfun(@(x) isnumeric(x) || isstring(x) || ischar(x) || iscell(x) || islogical(x), varargin));
+    mIp.addRequired("A", @(x) validateattributes(x, {'numeric', 'string', 'char', 'logical', 'struct'}, {'2d'}));
+    bIdx = find(cellfun(@(x) strcmpi(x, 'UniformOutput'), varargin)) - 1;
 
     for n = 1:length(bIdx)
         eval(['B', num2str(bIdx(n)), '=varargin{', num2str(bIdx(n)), '};']);
-        mIp.addOptional(eval(['"B', num2str(bIdx(n)), '"']), [], @(x) validateattributes(x, {'numeric', 'string', 'char', 'cell', 'logical'}, {'size', [size(A, 1), NaN]}));
+        mIp.addOptional(eval(['"B', num2str(bIdx(n)), '"']), [], @(x) validateattributes(x, {'numeric', 'string', 'char', 'logical', 'struct'}, {'size', [size(A, 1), NaN]}));
     end
 
-    mIp.addParameter("UniformOutput", true, @islogical);
+    mIp.addParameter("UniformOutput", true, @(x) isscalar(x) && (islogical(x) || ismember(x, [0, 1])));
     mIp.parse(fcn, A, varargin{:});
 
     %% Impl
