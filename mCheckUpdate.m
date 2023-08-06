96347bc109482bb86d0485e4c7b4b282a93c2451
TOMORI233 <ihatewo@qq.com>
2023-08-06 16:06:05 +0800

    add push to github

--- mCheckUpdate.m
+++ mCheckUpdate.m
@@ -1,6 +1,10 @@
-function mCheckUpdate(logstr)
+function mCheckUpdate(logstr, syncOpt)
     % Commit current work and check update for MATLABUtils
-    narginchk(0, 1);
+    narginchk(0, 2);
+
+    if nargin < 2
+        syncOpt = false;
+    end
     
     currentPath = pwd;
     cd(fileparts(mfilename("fullpath")));
@@ -8,13 +12,19 @@ function mCheckUpdate(logstr)
     currentUser = split(currentUser, '\');
     
     system("git add .");
-    if nargin < 1
+
+    if nargin < 1 || isempty(char(logstr))
         system(strcat("git commit -m ""update ", string(datetime), " by ", currentUser{1}, """"));
     else
         logstr = strrep(logstr, '"', '""');
         system(strcat("git commit -m """, logstr, """"));
     end
+
     system("git pull origin master");
+
+    if syncOpt
+        system("git push origin master");
+    end
     
     cd(currentPath);
     return;
