b35727d4ae799d5866f7a44145a9bbd08ae5e78a
TOMORI233 <ihatewo@qq.com>
2023-08-06 16:02:10 +0800

    update log input

--- mCheckUpdate.m
+++ mCheckUpdate.m
@@ -1,11 +1,21 @@
-% Commit current work and check update for MATLABUtils
-currentPath = pwd;
-cd(fileparts(mfilename("fullpath")));
-[~, currentUser] = system("whoami");
-currentUser = split(currentUser, '\');
-
-system("git add .");
-system(strcat("git commit -m ""update ", string(datetime), " by ", currentUser{1}, """"));
-system("git pull origin master");
-
-cd(currentPath);
+function mCheckUpdate(logstr)
+    % Commit current work and check update for MATLABUtils
+    narginchk(0, 1);
+    
+    currentPath = pwd;
+    cd(fileparts(mfilename("fullpath")));
+    [~, currentUser] = system("whoami");
+    currentUser = split(currentUser, '\');
+    
+    system("git add .");
+    if nargin < 1
+        system(strcat("git commit -m ""update ", string(datetime), " by ", currentUser{1}, """"));
+    else
+        logstr = strrep(logstr, '"', '""');
+        system(strcat("git commit -m """, logstr, """"));
+    end
+    system("git pull origin master");
+    
+    cd(currentPath);
+    return;
+end
