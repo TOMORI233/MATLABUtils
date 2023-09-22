eb33bc9701728306e436e312dd1df3c8cc457c17
TOMORI233 <ihatewo@qq.com>
2023-09-22 17:09:42 +0800

    update 2023/09/22 17:09:42:862 by tomori

--- data processing/statistics/findVectorLoc.m
+++ data processing/statistics/findVectorLoc.m
@@ -1,4 +1,16 @@
-function loc = findVectorLoc(X, pat)
-    loc = find(arrayfun(@(idx) isequal(X(idx:idx + length(pat) - 1), pat), 1:length(X) - length(pat)), 1);
+function locs = findVectorLoc(X, pat, n)
+    % Find location of vector [pat] in vector [X].
+    % [locs] is row vector of index numbers.
+    % [X] and [pat] are either row vectors or column vectors.
+    % If [n] is not specified, return all index numbers.
+    
+    narginchk(2, 3);
+
+    if nargin < 3
+        locs = find(arrayfun(@(idx) isequal(X(idx:idx + length(pat) - 1), pat), 1:length(X) - length(pat)));
+    else
+        locs = find(arrayfun(@(idx) isequal(X(idx:idx + length(pat) - 1), pat), 1:length(X) - length(pat)), n);
+    end
+
     return;
 end
