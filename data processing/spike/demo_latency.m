e9a269ecb8a6184c45f43db2fe78e7f2b19a55f4
TOMORI233 <ihatewo@qq.com>
2023-10-26 13:33:49 +0800

    update 2023/10/26 13:33:49:685 by tomori

--- data processing/spike/demo_latency.m
+++ data processing/spike/demo_latency.m
@@ -1,22 +1,29 @@
 ccc;
 
-load('D:\Education\Lab\Projects\Neural correlates with duration related cognition (pre)\SN_m4\noise duration\m4c2_duration_att30dB_sort_1.mat');
+load('D:\Education\Lab\Projects\Neural correlates with duration related cognition (pre)\SN_m4\noise duration\m4c24_duration_att30dB_sort_1.mat');
 
+%% 
 set(0, "defaultAxesFontSize", 12);
 
-windowOnset = [0, 30]; % ms
+windowOnset = [0, 2000]; % ms
 windowBase = [-100, 0]; % ms
 th = 1e-6;
 
+%% 
+trialAll = noiseProcessFcn(epocs);
+trialAll = selectSpikes(spiketime / 1000, trialAll, "trial onset", [-100, 2000]);
+trialAll = addfield(trialAll, "duration", epocs.dura.data);
+[~, idx] = sort([trialAll.duration], "ascend");
+trialAll = trialAll(idx);
+
+%%
 figure;
 maximizeFig;
 mSubplot(1, 2, 1, "shape", "square-min");
-trialAll = noiseProcessFcn(epocs);
-trialAll = selectSpikes(spiketime / 1000, trialAll, "trial onset", [-100, 1100]);
 rasterData.X = {trialAll.spike};
 mRaster(rasterData);
 xlabel('Time (ms)');
-xlim([-20, 50]);
+addLines2Axes(gca, struct("X", 0, "width", 2, "style", "-"));
 
 %%
 sprate = mean(calFR(trialAll, windowBase));
@@ -32,14 +39,14 @@ latency = spikes(find(P < th, 1) + 4);
 
 mSubplot(1, 2, 2, "nSize", [0.8, 0.2], "alignment", "top-center");
 plot(spikes, zeros(length(spikes), 1), "k", "Marker", "|", "LineStyle", "none", "MarkerSize", 20);
-xlim(windowOnset);
+xlim([0, 50]);
 yticklabels('');
 xlabel('Time (ms)');
 ylabel('Spikes');
 
 mSubplot(1, 2, 2, "nSize", [0.8, 0.6], "alignment", "bottom-center");
 plot(spikes(5:end), P, "k", "LineWidth", 2);
-xlim(windowOnset);
+xlim([0, 50]);
 xlabel('Time (ms)');
 ylabel('Probability');
 set(gca, "YScale", "log");
