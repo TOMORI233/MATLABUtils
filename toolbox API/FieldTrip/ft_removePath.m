1aab45e9802f1bda45e6fea9a504a3d7b5c49f92
TOMORI233 <ihatewo@qq.com>
2023-11-07 15:56:12 +0800

    update 2023/11/07 15:56:11:932 by tomori

--- toolbox API/FieldTrip/ft_removePath.m
+++ toolbox API/FieldTrip/ft_removePath.m
@@ -1,4 +1,6 @@
 function ft_removePath
+% remove FieldTrip paths but reserve root path of ft_defaults
+% run ft_defaults to restore FieldTrip paths
 
 try
     % find existing FieldTrip paths
@@ -6,19 +8,12 @@ try
     pathsAll = split(pathsAll, ';');
     toRm = pathsAll(contains(pathsAll, 'fieldtrip', 'IgnoreCase', true));
     
-    % startupPath = which("startup", "-all");
-    % mPath = regexp(startupPath, '.*FieldTrip', 'match');
-    % mPath = string(mPath{~cellfun(@isempty, mPath)});
-    % temp = dir(strcat(mPath, "\**\*"));
-    % mfolders = string({temp([temp.isdir]').folder}');
-    % p = string(strsplit(path, ";"))';
-    % toRm = cellstr(intersect(mfolders, p));
-    
     % clear existing FieldTrip paths
     rmpath(toRm{:});
 
-    % add path of current file and the root path of fieldtrip
-    
+    % add path of current file and the root path of ft_defaults
+    addpath(fileparts(mfilename("fullpath")));
+    addpath(fileparts(which("ft_defaults")));
 catch e
     disp(e.message);
 end
