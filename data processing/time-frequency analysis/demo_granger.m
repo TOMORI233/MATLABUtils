ca2d892875ea3328568e63a511c55c1faf5c8be0
TOMORI233 <ihatewo@qq.com>
2023-11-15 15:54:18 +0800

    update 2023/11/15 15:54:18:534 by tomori

--- data processing/time-frequency analysis/demo_granger.m
+++ data processing/time-frequency analysis/demo_granger.m
@@ -6,6 +6,8 @@ rng default
 rng(50)
 
 fs = 200;
+nperm = 500;
+alphaVal = 0.01;
 
 cfg             = [];
 cfg.ntrials     = 500;
@@ -79,6 +81,9 @@ xlabel('time (s)')
 %% wavelet transform
 [cwtres, f, coi] = cellfun(@(x) cwtMultiAll(x', fs), y, "UniformOutput", false);
 f = flip(f{1})';
+f = 10 * log(f);
+c = 0 - f(1);
+f = f + c;
 coi = coi{1};
 cwtres = cellfun(@(x) flip(x, 1), cwtres, "UniformOutput", false);
 cwtres = cellfun(@(x) permute(x, [4, 3, 1, 2]), cwtres, "UniformOutput", false);
@@ -98,18 +103,37 @@ grangercfg.channelcmb = cat(2, cellstr(repmat(freqdata.label{1}, [length(data.la
 grangercfg.cmbindx = [ones(size(grangercfg.channelcmb, 1), 1), [2:size(grangercfg.channelcmb, 1) + 1]'];
 gdata = ft_granger_hm(grangercfg, freqdata);
 
+%% PT
+grangerspctrm = zeros(length(data.label(2:end)) * 2, length(f), length(t), nperm + 1);
+grangerspctrm(:, :, :, 1) = gdata.grangerspctrm;
+for index = 1:nperm
+    % trial randomization
+    for chIdx = 1:size(freqdata.fourierspctrm, 2)
+        freqdata.fourierspctrm(:, chIdx, :, :) = freqdata.fourierspctrm(randperm(size(freqdata.fourierspctrm, 1)), chIdx, :, :);
+    end
+
+    gdata = ft_granger_hm(grangercfg, freqdata);
+    grangerspctrm(:, :, :, 1 + index) = gdata.grangerspctrm;
+end
+gdata.freq = exp((gdata.freq - c) / 10);
+
 %%
 figure;
 maximizeFig;
 
 mSubplot(2, 2, 1);
-imagesc("XData", gdata.time, "YData", flip(gdata.freq), "CData", flip(squeeze(gdata.grangerspctrm(2, :, :)), 2));
+P = squeeze(sum(grangerspctrm(2, :, :, 1) < grangerspctrm(2, :, :, 2:end), 4)) / nperm;
+P = reshape(P, [numel(P), 1]);
+P = mafdr(P, 'BHFDR', true);
+P = reshape(P, [length(f), length(t)]);
+temp = flip(squeeze(grangerspctrm(2, :, :, 1)) .* (P < alphaVal), 2);
+imagesc("XData", gdata.time, "YData", flip(gdata.freq), "CData", temp);
 set(gca, "XLimitMethod", "tight");
 set(gca, "YLimitMethod", "tight");
 title('From signal001 to signal002');
 
 mSubplot(2, 2, 2);
-imagesc("XData", gdata.time, "YData", flip(gdata.freq), "CData", flip(squeeze(gdata.grangerspctrm(4, :, :)), 2));
+imagesc("XData", gdata.time, "YData", flip(gdata.freq), "CData", flip(squeeze(grangerspctrm(4, :, :, 1)), 2));
 set(gca, "XLimitMethod", "tight");
 set(gca, "YLimitMethod", "tight");
 title('From signal001 to signal003');
