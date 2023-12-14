ba811b98ae1bdf387bd61509bbb19e9ee9a3b491
TOMORI233 <ihatewo@qq.com>
2023-12-14 18:37:30 +0800

    update 2023-12-14 18:37:30 by tomori

--- data structures/cell/cutData.m
+++ data structures/cell/cutData.m
@@ -2,7 +2,7 @@ function trialsData = cutData(trialsData, fs, windowOld, windowNew)
     % [windowOld] and [windowNew] are in ms
     % [fs] is in Hz
 
-    tIdx = fix((windowNew(1) - windowOld(1)) / 1000 * fs):fix((windowNew(2) - windowOld(1)) / 1000 * fs);
+    tIdx = fix((windowNew(1) - windowOld(1)) / 1000 * fs) + 1:fix((windowNew(2) - windowOld(1)) / 1000 * fs);
     trialsData = cellfun(@(x) x(:, tIdx), trialsData, "UniformOutput", false);
     return;
 end
