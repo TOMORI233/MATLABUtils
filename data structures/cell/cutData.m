7926264cde96aa4a9952fa4a43916aacf59fc768
TOMORI233 <ihatewo@qq.com>
2024-01-11 09:17:38 +0800

    update 2024-01-11 09:17:38 by tomori

--- data structures/cell/cutData.m
+++ data structures/cell/cutData.m
@@ -1,10 +1,18 @@
 function trialsData = cutData(trialsData, windowOld, windowNew)
     % [windowOld] and [windowNew] are in ms
 
-    idx = find(~cellfun(@isempty, trialsData));
-    t = linspace(windowOld(1), windowOld(2), size(trialsData{idx(1)}, 2));
-    tIdx = find(t >= windowNew(1), 1):find(t >= windowNew(2), 1);
-    trialsData(idx) = cellfun(@(x) x(:, tIdx), trialsData(idx), "UniformOutput", false);
+    if isa(trialsData, "double") % For single trial input
+        t = linspace(windowOld(1), windowOld(2), size(trialsData, 2));
+        tIdx = find(t >= windowNew(1), 1):find(t >= windowNew(2), 1);
+        trialsData = trialsData(:, tIdx);
+    elseif isa(trialsData, "cell")
+        idx = find(~cellfun(@isempty, trialsData));
+        t = linspace(windowOld(1), windowOld(2), size(trialsData{idx(1)}, 2));
+        tIdx = find(t >= windowNew(1), 1):find(t >= windowNew(2), 1);
+        trialsData(idx) = cellfun(@(x) x(:, tIdx), trialsData(idx), "UniformOutput", false);
+    else
+        error("Invalid data type");
+    end
 
     return;
 end
