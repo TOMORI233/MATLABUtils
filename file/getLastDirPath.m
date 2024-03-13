25ed0bf84c3e0e0cd390a18e0168b63593575518
TOMORI233 <ihatewo@qq.com>
2024-03-13 23:12:18 +0800

    update 2024-03-13 23:12:18 by xhx

--- file/getLastDirPath.m
+++ file/getLastDirPath.m
@@ -1,5 +1,20 @@
-function res = getLastDirPath(fullPath, N)
-    res = fileparts(fullPath); % get folder
-    temp = split(res, '\');
+function [res, folderNames] = getLastDirPath(P, N)
+    % [res] return the last end-N+1:end folder path of path P
+    % [folderNames] return all folder names in [res] (from upper to lower)
+
+    if N <= 0
+        error("getLastDirPath(): input N should be positive integer");
+    end
+
+    [FILEPATH, ~, EXT] = fileparts(P);
+
+    if isempty(EXT) % P is folder path
+        temp = split(P, '\');
+    else % P is full path of a file
+        temp = split(FILEPATH, '\');
+    end
+    
     res = fullfile(temp{end - N + 1:end});
+    folderNames = temp(end - N + 1:end);
+    return;
 end
