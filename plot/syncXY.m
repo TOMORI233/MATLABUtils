9b5101c72e1067c70bf4c1c31f3cf894543c53b2
TOMORI233 <ihatewo@qq.com>
2024-03-19 18:36:08 +0800

    update 2024-03-19 18:36:08 by tomori

---
+++ plot/syncXY.m
@@ -0,0 +1,14 @@
+function syncXY(mAxe)
+    narginchk(0, 1);
+
+    if nargin < 1
+        mAxe = gca;
+    end
+
+    xRange = get(mAxe, "XLim");
+    yRange = get(mAxe, "YLim");
+    xyRange = [min([xRange, yRange]), max([xRange, yRange])];
+    set(mAxe, "XLim", xyRange);
+    set(mAxe, "YLim", xyRange);
+    return;
+end
