792cac91eb1827bb1ef7eb1e97c7b103580fc6e8
TOMORI233 <ihatewo@qq.com>
2024-03-19 21:16:34 +0800

    update 2024-03-19 21:16:34 by tomori

---
+++ data processing/statistics/maxt.m
@@ -0,0 +1,14 @@
+function [maxVal, t] = maxt(X, T, dim)
+    % return maximum value of time series data X and the corresponding time t
+
+    narginchk(2, 3);
+
+    if nargin < 3
+        [maxVal, idx] = max(X);
+    else
+        [maxVal, idx] = max(X, [], dim);
+    end
+
+    t = T(idx);
+    return;
+end
