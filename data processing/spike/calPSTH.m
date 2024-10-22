fb56a7b6fa0899f4e06d54f18559cb921cb3dea1
TOMORI233 <ihatewo@qq.com>
2024-10-22 10:05:11 +0800

    update 2024-10-22 10:05:11 by tomori

--- data processing/spike/calPSTH.m
+++ data processing/spike/calPSTH.m
@@ -1,6 +1,7 @@
 function [psth, edge, whole] = calPSTH(trials, windowPSTH, binSize, step)
     % If [trials] is a struct array, it should contain field [spike] for each trial.
     % If [trials] is a cell array, its element contains spikes of each trial.
+    % If [trials] is a numeric vector, it represents spike times in one trial.
     % [psth] will be returned as a column vector.
     % [windowPSTH] is a two-element vector in millisecond.
     % [binSize] and [step] are in millisecond.
@@ -18,6 +19,12 @@ function [psth, edge, whole] = calPSTH(trials, windowPSTH, binSize, step)
         case "struct"
             temp = arrayfun(@(x) reshape(x.spike, [numel(x.spike), 1]), trials, "UniformOutput", false);
             psth = mHist(vertcat(temp), edge, binSize) / (binSize / 1000) / length(trials); % Hz
+        case "double"
+            if isvector(trials)
+                psth = mHist(trials, edge, binSize) / (binSize / 1000);
+            else
+                error("Invalid trials input");
+            end
     end
     whole = [edge', psth];
     return;
