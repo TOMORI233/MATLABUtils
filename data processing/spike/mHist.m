39486e3325e945ab0b44561c420adf1153454d7a
TOMORI233 <ihatewo@qq.com>
2024-11-02 15:49:55 +0800

    update 2024-11-02 15:49:55 by tomori

--- data processing/spike/mHist.m
+++ data processing/spike/mHist.m
@@ -1,9 +1,14 @@
-function h = mHist(data, edge, binSize)
+function counts = mHist(data, edge, binSize)
     % [edge(idx) - binSize / 2, edge(idx) + binSize / 2)
-    h = zeros(length(edge), 1);
 
-    for index = 1:length(edge)
-        h(index) = length(find(data >= edge(index) - binSize / 2 & data < edge(index) + binSize / 2));
+    if isreal(data) && isvector(data)
+        data = repmat(data(:)', [numel(edge), 1]);
+        leftBorder = edge(:) - binSize / 2;
+        rightBorder = edge(:) + binSize / 2;
+        res = data >= leftBorder & data < rightBorder;
+        counts = sum(res, 2);
+    else
+        error("data should be a vector");
     end
 
     return;
