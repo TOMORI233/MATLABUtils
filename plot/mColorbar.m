d2b0e5f108fce5bb40e063e23f12ccacb7b5e882
TOMORI233 <ihatewo@qq.com>
2024-11-27 12:58:33 +0800

    local

--- plot/mColorbar.m
+++ plot/mColorbar.m
@@ -2,11 +2,9 @@ function cb = mColorbar(varargin)
 % Create a colorbar outside the tightPosition("IncludeLabels", true)
 % Use `colorbar` for input hint and modify function name to `mColorbar`
 
-% Find "location" input
-idx = cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Location"), varargin);
-
-if any(idx)
-    loc = varargin{find(idx, 1) + 1};
+if nargin == 0 % no input
+    ax = gca;
+else
 
     if strcmp(class(varargin{1}), "matlab.graphics.axis.Axes")
         ax = varargin{1};
@@ -14,20 +12,31 @@ if any(idx)
         ax = gca;
     end
 
-    pos0 = tightPosition(ax, "IncludeLabels", true);
-    pos = get(ax, "Position"); % axes size
-    otherParams = varargin(~idx);
+end
 
-    switch loc
-        case "northoutside"
-            cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) + pos0(4) * 1.05, pos(3), pos0(4) * 0.05]);
-        case "southoutside"
-            cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) - pos0(4) * 0.1, pos(3), pos0(4) * 0.05]);
-        case "eastoutside"
-            cb = colorbar(otherParams{:}, "Position", [pos0(1) + pos0(3) * 1.05, pos(2), pos0(3) * 0.05, pos(4)]);
-        case "westoutside"
-            cb = colorbar(otherParams{:}, "Position", [pos0(1) - pos0(3) * 0.1, pos(2), pos0(3) * 0.05, pos(4)]);
-    end
+% Find "location" input
+idx = cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Location"), varargin);
+
+if ~any(idx)
+    loc = "eastoutside";
+else
+    loc = varargin{find(idx, 1) + 1};
+end
+
+% Create colorbar
+pos0 = tightPosition(ax, "IncludeLabels", true);
+pos = get(ax, "Position"); % axes size
+otherParams = varargin(~idx);
+
+switch loc
+    case "northoutside"
+        cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) + pos0(4) * 1.05, pos(3), pos0(4) * 0.05]);
+    case "southoutside"
+        cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) - pos0(4) * 0.1, pos(3), pos0(4) * 0.05]);
+    case "eastoutside"
+        cb = colorbar(otherParams{:}, "Position", [pos0(1) + pos0(3) * 1.05, pos(2), pos0(3) * 0.05, pos(4)]);
+    case "westoutside"
+        cb = colorbar(otherParams{:}, "Position", [pos0(1) - pos0(3) * 0.1, pos(2), pos0(3) * 0.05, pos(4)]);
 end
 
 return;
