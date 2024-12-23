99fae8b2f9ab5d490d63e4b0a94817038db1126e
TOMORI233 <ihatewo@qq.com>
2024-12-23 16:45:14 +0800

    update 2024-12-23 16:45:14 by xhx

--- plot/mColorbar.m
+++ plot/mColorbar.m
@@ -29,13 +29,13 @@ otherParams = varargin(~idx);
 
 switch loc
     case "northoutside"
-        cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) + pos0(4) * 1.05, pos(3), pos0(4) * 0.05]);
+        cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) + pos0(4) * 1.01, pos(3), pos0(4) * 0.025]);
     case "southoutside"
-        cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) - pos0(4) * 0.1, pos(3), pos0(4) * 0.05]);
+        cb = colorbar(otherParams{:}, "Position", [pos(1), pos0(2) - pos0(4) * 0.05, pos(3), pos0(4) * 0.025]);
     case "eastoutside"
-        cb = colorbar(otherParams{:}, "Position", [pos0(1) + pos0(3) * 1.05, pos(2), pos0(3) * 0.05, pos(4)]);
+        cb = colorbar(otherParams{:}, "Position", [pos0(1) + pos0(3) * 1.01, pos(2), pos0(3) * 0.025, pos(4)]);
     case "westoutside"
-        cb = colorbar(otherParams{:}, "Position", [pos0(1) - pos0(3) * 0.1, pos(2), pos0(3) * 0.05, pos(4)]);
+        cb = colorbar(otherParams{:}, "Position", [pos0(1) - pos0(3) * 0.05, pos(2), pos0(3) * 0.025, pos(4)]);
 end
 
 return;
