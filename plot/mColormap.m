7ccf25d010b25ccb0893687910c6750f31bc59f0
TOMORI233 <ihatewo@qq.com>
2025-02-10 11:44:58 +0800

    update 2025-02-10 11:44:58 by xhx

--- plot/mColormap.m
+++ plot/mColormap.m
@@ -13,17 +13,17 @@ end
 cHead = validatecolor(head);
 cTail = validatecolor(tail);
 
-[h1, s1, v1] = rgb2hsv(cHead);
-[~, s2, v2] = rgb2hsv([1, 1, 1]);
-[h3, s3, v3] = rgb2hsv(cTail);
+hsv1 = rgb2hsv(cHead);
+hsv2 = rgb2hsv([1, 1, 1]);
+hsv3 = rgb2hsv(cTail);
 
-S_head = linspace(s1, s2, 128)';
-V_head = linspace(v1, v2, 128)';
-S_tail = linspace(s2, s3, 128)';
-V_tail = linspace(v2, v3, 128)';
+S_head = linspace(hsv1(2), hsv2(2), 128)';
+V_head = linspace(hsv1(3), hsv2(3), 128)';
+S_tail = linspace(hsv2(2), hsv3(2), 128)';
+V_tail = linspace(hsv2(3), hsv3(3), 128)';
 
-H_head = repmat(h1, [128, 1]);
-H_tail = repmat(h3, [128, 1]);
+H_head = repmat(hsv1(1), [128, 1]);
+H_tail = repmat(hsv3(1), [128, 1]);
 
 cm = [H_head, S_head, V_head; ...
       H_tail, S_tail, V_tail];
