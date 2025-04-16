855ce9a999ae7167203a8349600a093456a96809
TOMORI233 <ihatewo@qq.com>
2025-04-16 17:16:44 +0800

    update 2025-04-16 17:16:44 by tomori

---
+++ file/exportcolorbar.m
@@ -0,0 +1,28 @@
+function exportcolorbar(cRange, SAVEPATH, varargin)
+mIp = inputParser;
+mIp.addRequired("cRange", @(x) validateattributes(x, {'numeric'}, {'numel', 2, 'increasing'}));
+mIp.addRequired("SAVEPATH", @(x) isStringScalar(x) || ischar(x));
+mIp.addOptional("Colormap", 'jet', @(x) isStringScalar(x) || ischar(x) || (isreal(x) && isequal(size(x), [256, 3])));
+mIp.addParameter("ShowZero", true, @(x) isscalar(x) && islogical(x));
+mIp.parse(cRange, SAVEPATH, varargin{:});
+colorMap = mIp.Results.Colormap;
+ShowZero = mIp.Results.ShowZero;
+
+Fig = figure("WindowState", "maximized");
+ax = mSubplot(Fig, 1, 1, 1);
+cb = colorbar(ax, "Location", "eastoutside");
+cb.FontSize = 14;
+cb.FontWeight = "bold";
+clim(ax, cRange);
+
+if ShowZero && cRange(1) < 0 && cRange(2) > 0
+    cb.Ticks = [cRange(1), 0, cRange(2)];
+end
+
+colormap(ax, colorMap);
+set(ax, "Visible", "off");
+exportgraphics(ax, SAVEPATH, "Resolution", 300);
+close(Fig);
+
+return;
+end
--- plot/mColorbar.m
+++ plot/mColorbar.m
@@ -9,9 +9,9 @@ if isempty(varargin)
     widthIdx = [];
     ax = gca;
 else
-    locIdx = cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Location"), varargin);
-    intervalIdx = cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Interval"), varargin);
-    widthIdx = cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Width"), varargin);
+    locIdx = find(cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Location"), varargin), 1);
+    intervalIdx = find(cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Interval"), varargin), 1);
+    widthIdx = find(cellfun(@(x) (ischar(x) || isStringScalar(x)) && strcmpi(x, "Width"), varargin), 1);
     
     if strcmp(class(varargin{1}), "matlab.graphics.axis.Axes")
         ax = varargin{1};
@@ -21,29 +21,25 @@ else
     
 end
 
-if any(locIdx)
-    idx = find(locIdx, 1);
-    loc = varargin{idx + 1};
-    varargin(idx:idx + 1) = [];
+if ~isempty(locIdx)
+    loc = varargin{locIdx + 1};
 else
     loc = "eastoutside";
 end
 
-if any(intervalIdx)
-    idx = find(intervalIdx, 1);
-    interval = varargin{idx + 1};
-    varargin(idx:idx + 1) = [];
+if ~isempty(intervalIdx)
+    interval = varargin{intervalIdx + 1};
 else
     interval = 0.01;
 end
 
-if any(widthIdx)
-    idx = find(widthIdx, 1);
-    width = varargin{idx+ 1};
-    varargin(idx:idx + 1) = [];
+if ~isempty(widthIdx)
+    width = varargin{widthIdx+ 1};
 else
     width = 0.01;
 end
+
+varargin([locIdx:locIdx + 1, intervalIdx:intervalIdx + 1, widthIdx:widthIdx + 1]) = [];
 
 pos0 = tightPosition(ax, "IncludeLabels", true);
 pos = get(ax, "Position"); % axes size
