c79c7c3f99502714ead9945ab5dc11e36cbf0715
TOMORI233 <ihatewo@qq.com>
2025-04-29 10:28:01 +0800

    update 2025-04-29 10:28:01 by tomori

--- plot/mColormap.m
+++ plot/mColormap.m
@@ -1,6 +1,6 @@
-function cm = mColormap(head, tail)
+function cm = mColormap(head, tail, n)
 % use white as middle
-narginchk(0, 2);
+narginchk(0, 3);
 
 if nargin < 1
     head = 'b';
@@ -10,6 +10,10 @@ if nargin < 2
     tail = 'r';
 end
 
+if nargin < 3
+    n = 1 / 2;
+end
+
 cHead = validatecolor(head);
 cTail = validatecolor(tail);
 
@@ -17,10 +21,12 @@ hsv1 = rgb2hsv(cHead);
 hsv2 = rgb2hsv([1, 1, 1]);
 hsv3 = rgb2hsv(cTail);
 
-S_head = linspace(hsv1(2), hsv2(2), 128)';
-V_head = linspace(hsv1(3), hsv2(3), 128)';
-S_tail = linspace(hsv2(2), hsv3(2), 128)';
-V_tail = linspace(hsv2(3), hsv3(3), 128)';
+x = linspace(0, 1, 128)';
+S_head = (hsv2(2) - hsv1(2)) * x .^ n + hsv1(2);
+V_head = (hsv2(3) - hsv1(3)) * x .^ n + hsv1(3);
+
+S_tail = flip((hsv2(2) - hsv3(2)) * x .^ n + hsv3(2));
+V_tail = flip((hsv2(3) - hsv3(3)) * x .^ n + hsv3(3));
 
 H_head = repmat(hsv1(1), [128, 1]);
 H_tail = repmat(hsv3(1), [128, 1]);
