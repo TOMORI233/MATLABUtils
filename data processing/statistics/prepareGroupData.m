eaf76914a5c0098c22f958e6f9eb43bf224e40f1
TOMORI233 <ihatewo@qq.com>
2025-04-29 16:56:46 +0800

    update 2025-04-29 16:56:45 by tomori

--- data processing/statistics/prepareGroupData.m
+++ data processing/statistics/prepareGroupData.m
@@ -1,4 +1,4 @@
-function res = prepareGroupData(varargin)
+function [res, tbl] = prepareGroupData(varargin)
 
 N = cellfun(@numel, varargin);
 varargin = cellfun(@(x) x(:), varargin, "UniformOutput", false);
@@ -6,6 +6,7 @@ X = cat(1, varargin{:});
 G = arrayfun(@(x, y) y * ones(x, 1), N, 1:length(N), "UniformOutput", false);
 G = cat(1, G{:});
 res = [X, G];
+tbl = table(X(:), categorical(string(char('A' + G - 1))), 'VariableNames', {'y', 'group'});
 
 return;
 end
